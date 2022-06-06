43915a71d54e83d1b4cc80928757209285691d13
Henry Ben Yakov <henrybenyakov1@gmail.com>
2022-06-06 16:08:16 +0300

    build complete

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com'],
+  },
+  env: {
+    MapboxAccessToken: 'pk.eyJ1IjoiaGVucnliZW55YWtvdiIsImEiOiJjbDQxa2xmbTgxOGdvM2JtazN1cjVrdGpuIn0.5H3AAox5IEim7p4BPd1MDA'
+  }
 }
 
 module.exports = nextConfig
